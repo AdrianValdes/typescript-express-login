15f0015b5ead0f6386045f98aa45f4a907622ed9
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-14 10:26:50 +0100

    add dummy login functionality

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -4,9 +4,6 @@ exports.router = void 0;
 var express_1 = require("express");
 var router = express_1.Router();
 exports.router = router;
-router.get('/', function (req, res) {
-    res.send("<div>\n      <h1>Hi from express on port 5005</h1>\n    </div>");
-});
 router.get('/login', function (req, res) {
     res.send("\n    <form method=\"POST\">\n        <div>\n            <label>Email</label>\n            <input name=\"email\" autocomplete=\"off\"/>\n        </div>\n        <div> \n            <label>Password</label>\n            <input name=\"password\" type=\"password\" />\n        </div>\n        <button> Submit </button>\n    </form>\n  ");
 });
@@ -18,5 +15,13 @@ router.post('/login', function (req, res) {
     }
     else {
         res.send('Invalid email or password');
+    }
+});
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n    <div>\n      <div> You are logged in </div>\n      <a href=\"/logout\"> Logout </a>\n    <div>\n    ");
+    }
+    else {
+        res.send("\n    <div>\n      <div> You are not logged in </div>\n      <a href=\"/login\"> Login </a>\n    <div>\n    ");
     }
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -6,14 +6,6 @@ interface RequestWithBody extends Request {
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send(
-    `<div>
-      <h1>Hi from express on port 5005</h1>
-    </div>`
-  );
-});
-
 router.get('/login', (req: Request, res: Response) => {
   res.send(`
     <form method="POST">
@@ -38,6 +30,24 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
     res.redirect('/');
   } else {
     res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div>
+      <div> You are logged in </div>
+      <a href="/logout"> Logout </a>
+    <div>
+    `);
+  } else {
+    res.send(`
+    <div>
+      <div> You are not logged in </div>
+      <a href="/login"> Login </a>
+    <div>
+    `);
   }
 });
 
